42dd9f5e4d0fd9922afaf434715a6a924719eda0
Remi <remi.xboxlive@hotmail.com>
2020-01-21 23:36:41 -0500

    entry-point html/css not completed

--- cadriciel/client/src/app/app.module.ts
+++ cadriciel/client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EntryPointComponent } from './components/entry-point/entry-point.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+    declarations: [AppComponent, EntryPointComponent],
+    imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatButtonModule, MatIconModule,],
     providers: [],
     bootstrap: [AppComponent],
 })
