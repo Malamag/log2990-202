2ef8de7c90abc14d390879764ec67e2b62c4b803
Remi <remi.xboxlive@hotmail.com>
2020-01-22 20:31:35 -0500

    added snackbar to welcome users

--- cadriciel/client/src/app/app.module.ts
+++ cadriciel/client/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EntryPointComponent } from './components/entry-point/entry-point.component';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 @NgModule({
     declarations: [AppComponent, EntryPointComponent],
-    imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatButtonModule, MatIconModule,],
+    imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatButtonModule, MatIconModule,MatSnackBarModule,],
     providers: [],
     bootstrap: [AppComponent],
 })
--- cadriciel/client/src/app/components/entry-point/entry-point.component.ts
+++ cadriciel/client/src/app/components/entry-point/entry-point.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { MatSnackBar,MatSnackBarConfig} from '@angular/material';
 @Component({
   selector: 'app-entry-point',
   templateUrl: './entry-point.component.html',
   styleUrls: ['./entry-point.component.scss']
 })
 export class EntryPointComponent implements OnInit {
-
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.open();
   }
-
+  
+  open() {
+    let config = new MatSnackBarConfig();
+    config.duration=2500;
+    this.snackBar.open("Bienvenue sur PolyDessin !",undefined,config);
+  }
+  
 }
