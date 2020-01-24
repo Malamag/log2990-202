725b3cce61af0a6916d0dbcbbaa9b19940e9f558
Remi <remi.lepage@polymtl.ca>
2020-01-24 12:56:32 -0500

    added functionality array to simplify DOM

--- cadriciel/client/src/app/components/entry-point/entry-point.component.ts
+++ cadriciel/client/src/app/components/entry-point/entry-point.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar,MatSnackBarConfig} from '@angular/material';
+import { functionality } from '../../functionality';
+
 @Component({
   selector: 'app-entry-point',
   templateUrl: './entry-point.component.html',
   styleUrls: ['./entry-point.component.scss']
 })
 export class EntryPointComponent implements OnInit {
+  functionality = functionality
   constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit() {
---
+++ cadriciel/client/src/app/functionality.ts
@@ -0,0 +1,30 @@
+export const functionality = [
+    {
+      name: "Créer un nouveau dessin",
+      shortcutName: "Créer",
+      shortDescription:"text goes here",
+      description: "text goes here",
+      icon :"add_circle",  
+    },
+    {
+        name: "Ouvrir la galerie de dessins disponibles sur le serveur",
+        shortcutName: "Ouvrir",
+        shortDescription:"text goes here",
+        description: "text goes here",
+        icon :"camera",  
+    },
+    {
+        name: "Afficher le guide d'utilisation de l'application",
+        shortcutName: "Afficher",
+        shortDescription:"text goes here",
+        description: "text goes here",
+        icon :"menu_book",  
+    },
+    {
+        name: "Continuer un dessin",
+        shortcutName: "Continuer",
+        shortDescription:"text goes here",
+        description: "text goes here",
+        icon :"arrow_forward",  
+      },
+  ];
