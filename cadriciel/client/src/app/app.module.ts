8c9ae71a5f282f801ddcf66a3d9ffea3511a4d96
Wasabi340 <Wasabi340@users.noreply.github.com>
2020-01-23 19:03:28 -0500

    component test

--- cadriciel/client/src/app/app.module.ts
+++ cadriciel/client/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
+import { SvgDrawComponent } from './components/svg-draw/svg-draw.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, SvgDrawComponent],
     imports: [BrowserModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
---
+++ cadriciel/client/src/app/components/svg-draw/svg-draw.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-draw',
+  templateUrl: './svg-draw.component.html',
+  styleUrls: ['./svg-draw.component.scss']
+})
+export class SvgDrawComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
