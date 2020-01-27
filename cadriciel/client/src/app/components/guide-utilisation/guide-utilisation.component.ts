350db67f939a1e2d56fb1025bfdb6ecf933b6cd9
Jérémy Charland <jeremy.charland@polymtl.ca>
2020-01-27 09:10:52 -0500

    Better buttons

--- cadriciel/client/src/app/components/guide-utilisation/guide-utilisation.component.ts
+++ cadriciel/client/src/app/components/guide-utilisation/guide-utilisation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 ////////////////////////
 import { MatDialogRef } from '@angular/material/dialog';
+import { functionality } from '../../functionality'
 
 @Component({
   selector: 'app-guide-utilisation',
@@ -9,7 +10,8 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./guide-utilisation.component.scss']
 })
 export class GuideUtilisationComponent implements OnInit {
-
+  functionality = functionality;
+  element: HTMLElement;
   constructor(public dialogRef: MatDialogRef<GuideUtilisationComponent>) { }
 
   ngOnInit() {
@@ -17,5 +19,21 @@ export class GuideUtilisationComponent implements OnInit {
 
   closeModal() {
     this.dialogRef.close();
+  }
+
+  toolsListButton(){
+    const tools = document.getElementsByClassName('toolButton');
+    var i;
+    for (i = 0; i < tools.length; i++) {
+      const tool = tools[i] as HTMLElement;
+      if (tool) {
+        if (tool.style.display == "block") {
+          tool.style.display = "none";
+          //tool.style.backgroundColor = 'grey';
+        }
+        else
+        tool.style.display = "block";
+      }
+    }      
   }
 }
