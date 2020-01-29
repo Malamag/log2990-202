308ea587b7eba28163a307432d554c9bbda2da1c
Alexandre Trepanier <Wasabi340@users.noreply.github.com>
2020-01-28 20:47:21 -0500

    textured brush

--- cadriciel/client/src/app/services/draw-tool/draw-tool.service.ts
+++ cadriciel/client/src/app/services/draw-tool/draw-tool.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable} from '@angular/core';
 import { KeyboardHandlerService } from '../keyboard-handler/keyboard-handler.service';
 
 @Injectable({
@@ -22,6 +22,10 @@ export class DrawToolService {
 
   CreateLine(_svg:HTMLElement | null,_workingSpace:HTMLElement | null, mouseX:number, mouseY:number,selected:boolean, width:number, primary_color:number,showJunctions:boolean){
     return new LineTool(_svg,_workingSpace,mouseX,mouseY,selected,width,primary_color, showJunctions);
+  }
+
+  CreateBrush(_svg:HTMLElement | null,_workingSpace:HTMLElement | null, mouseX:number, mouseY:number,selected:boolean, width:number, primary_color:number){
+    return new BrushTool(_svg,_workingSpace,mouseX,mouseY,selected,width,primary_color);
   }
 }
 
@@ -92,7 +96,7 @@ class PencilTool extends DrawingTool{
 
   down(){
     //mouse down with pencil in hand
-    if(this.mouseX - this._svgBox.left + (this._workingSpace? this._workingSpace.scrollLeft : 0) >= this._svgBox.left && this.mouseY - this._svgBox.top + (this._workingSpace? this._workingSpace.scrollTop : 0) >= this._svgBox.top){
+    if(this.mouseX + (this._workingSpace? this._workingSpace.scrollLeft : 0) >= this._svgBox.left && this.mouseY + (this._workingSpace? this._workingSpace.scrollTop : 0) >= this._svgBox.top){
       this.startedInsideWorkSpace = true;
     }else{
       this.startedInsideWorkSpace = false;
@@ -385,5 +389,113 @@ class LineTool extends DrawingTool{
     s+="\" stroke=\"red\" stroke-width=\"1\" fill=\"none\" stroke-linecap=\"round\" stroke-linejoin=\"round\" />";
   
     return s;
+  }
+}
+
+class BrushTool extends DrawingTool{
+  
+  constructor(_svg:HTMLElement | null, _workingSpace:HTMLElement | null,mouseX:number,mouseY:number,selected:boolean, width:number, primary_color:number){
+    super(_svg, _workingSpace, mouseX, mouseY,selected,width,primary_color);
+  }
+
+  update(keyboard:KeyboardHandlerService){
+    console.log("update on brush");
+  }
+
+  down(){
+    //mouse down with brush in hand
+    if(this.mouseX + (this._workingSpace? this._workingSpace.scrollLeft : 0) >= this._svgBox.left && this.mouseY + (this._workingSpace? this._workingSpace.scrollTop : 0) >= this._svgBox.top){
+      this.startedInsideWorkSpace = true;
+      console.log("yes");
+    }else{
+      this.startedInsideWorkSpace = false;
+    }
+    if(this.startedInsideWorkSpace){
+      console.log("-----CLICKED WITH BRUSH-----");
+      this.isDown = true;
+      this.currentX = this.mouseX - this._svgBox.left + (this._workingSpace? this._workingSpace.scrollLeft : 0);
+      this.currentY = this.mouseY - this._svgBox.top + (this._workingSpace? this._workingSpace.scrollTop : 0);
+      this.currentPath.push(new Point(this.currentX,this.currentY));
+      this.currentPath.push(new Point(this.currentX,this.currentY));
+      let d : string = "";
+      d+= this.createPath(this.currentPath);
+      /*
+      if(this._svg){
+        
+      }*/
+      document.getElementsByName("in-progress")[0].innerHTML = d;
+    }
+  }
+  up(){
+    //mouse up with brush in hand
+    if(this.startedInsideWorkSpace){
+      console.log("-----RELEASED BRUSH-----");
+      this.isDown = false;
+      let d : string = "";
+      d+= this.createPath(this.currentPath);
+      document.getElementsByName("drawing")[0].innerHTML += "<g name=\"brush-stroke\">" + d + "</g>";
+      document.getElementsByName("in-progress")[0].innerHTML = "";
+      this.currentPath = [];
+    }
+  }
+  move(){
+    //mouse move with brush in hand
+    if(this.startedInsideWorkSpace){
+      this.currentX = this.mouseX - this._svgBox.left + (this._workingSpace? this._workingSpace.scrollLeft : 0);
+      this.currentY = this.mouseY - this._svgBox.top + (this._workingSpace? this._workingSpace.scrollTop : 0);
+
+      if(this.isDown){
+        if(this.currentPath[this.currentPath.length-1].x != this.currentX || this.currentPath[this.currentPath.length-1].y != this.currentY){
+          this.currentPath.push(new Point(this.currentX, this.currentY));
+        }
+
+       // if(this.currentPath[this.currentPath.length-1].x == this.currentPath[this.currentPath.length-2].x || this.currentPath[this.currentPath.length-1].y == this.currentPath[this.currentPath.length-2].y){
+         // this.currentPath[this.currentPath.length-2] = this.currentPath[this.currentPath.length-1];
+         // this.currentPath.pop();
+        //}
+
+        let d : string = "";
+        d+= this.createPath(this.currentPath);
+        document.getElementsByName("in-progress")[0].innerHTML = d;
+      }
+    }
+  }
+  doubleClick(){
+    //mouse doubleClick with brush in hand
+    this.down();
+    this.up();
+    this.down();
+    this.up();
+  }
+  createPath(p:Point[]){
+
+    //p = this.Wrap(p,40);
+
+    let s = "<filter id=\"blurMe\" filterUnits=\"userSpaceOnUse\"><feGaussianBlur in=\"SourceGraphic\" stdDeviation=\"10\"/></filter>";
+    //let s = "<filter id=\"displacementFilter\" x=\"-100%\" y=\"-100%\" width=\"200%\" height=\"200%\" filterUnits=\"userSpaceOnUse\"><feTurbulence type=\"turbulence\" baseFrequency=\"0.05\"numOctaves=\"2\" result=\"turbulence\"/><feDisplacementMap in2=\"turbulence\" in=\"SourceGraphic\"scale=\"50\" xChannelSelector=\"B\" yChannelSelector=\"R\"/></filter>";
+
+    s += "<path d=\"";
+      s+= `M ${p[0].x-0/4} ${p[0].y-0/4} `;
+
+      for(let i = 1; i < p.length;i++){
+
+
+        s+= `L ${p[i].x-0/4} ${p[i].y-0/4} `;
+      }
+
+      s+="\" stroke=\"red\" stroke-width=\"10\" fill=\"none\" stroke-linecap=\"round\" stroke-linejoin=\"round\" filter=\"url(#blurMe)\" />";
+
+    return s;
+  }
+
+  Wrap(p:Point[], width:number){
+
+    let wrap : Point[] = [];
+
+    p.forEach(element => {
+      
+    });
+  
+    return wrap;
   }
 }
