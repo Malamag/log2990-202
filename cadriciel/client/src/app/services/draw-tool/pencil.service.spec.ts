1e41dfd4241944758a8b1b258f760d42cb72307a
Etienne <etienne.lescarbeault@polymtl.ca>
2020-02-06 15:30:31 -0500

    testbed conf - 7 to go!

--- cadriciel/client/src/app/services/draw-tool/pencil.service.spec.ts
+++ cadriciel/client/src/app/services/draw-tool/pencil.service.spec.ts
@@ -1,13 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PencilService } from './pencil.service';
+import { InteractionService } from '../service-interaction/interaction.service';
 
-
+export class fakeInteractionService extends InteractionService{}
 describe('PencilService', () => {
   beforeEach(() => {
 
     TestBed.configureTestingModule({
-    providers:[PencilService]
+    providers:[
+      PencilService,
+      {provide: Boolean, useValue: true},
+      {provide: String, useValue: ""},
+      {provide: Number, useValue: 0},
+      {provide: HTMLElement},
+      {provide: InteractionService, useClass: fakeInteractionService}]
     
   });
 });
--- cadriciel/client/src/app/services/draw-tool/rectangle.service.spec.ts
+++ cadriciel/client/src/app/services/draw-tool/rectangle.service.spec.ts
@@ -2,10 +2,20 @@ import { TestBed } from '@angular/core/testing';
 
 import { RectangleService } from './rectangle.service';
 import { Point } from './point';
+import { InteractionService } from '../service-interaction/interaction.service';
+
+export class fakeInteractionService extends InteractionService{}
 
 describe('RectangleService', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    providers:[{provide:Point}, {provide: HTMLElement}]
+    providers:[
+      RectangleService,
+      {provide:Point}, 
+      {provide: HTMLElement},
+      {provide: Number, useValue: 0},
+      {provide: String, useValue: ""},
+      {provide: Boolean, useValue: true},
+      {provide: InteractionService, useClass: fakeInteractionService}]
   }));
 
   it('should be created', () => {
