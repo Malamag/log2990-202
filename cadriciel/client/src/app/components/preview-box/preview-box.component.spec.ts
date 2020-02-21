8a53ae74d4e93617320d42b6a9a22e5771513250
Etienne <etienne.lescarbeault@polymtl.ca>
2020-02-21 15:57:09 -0500

    testbed fix on preview box

--- cadriciel/client/src/app/components/preview-box/preview-box.component.spec.ts
+++ cadriciel/client/src/app/components/preview-box/preview-box.component.spec.ts
@@ -2,30 +2,38 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PreviewBoxComponent } from './preview-box.component';
 import { DoodleFetchService } from 'src/app/services/doodle-fetch/doodle-fetch.service';
+import { SvgDrawComponent } from '../draw-view/svg-draw/svg-draw.component';
 
 describe('PreviewBoxComponent', () => {
   let component: PreviewBoxComponent;
   let fixture: ComponentFixture<PreviewBoxComponent>;
   let dFetchStub: any;
+
   beforeEach(async(() => {
+
     dFetchStub = {
       askForDoodle:()=>0,
       getDrawing:()=>undefined
     }
+
     TestBed.configureTestingModule({
-      declarations: [ PreviewBoxComponent ],
-      providers: [{provide: DoodleFetchService, dFetchStub}]
+      declarations: [ PreviewBoxComponent, SvgDrawComponent ],
+      providers: [
+        {provide: DoodleFetchService, useValue: dFetchStub}
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    dFetchStub.askForDoodle();
     fixture = TestBed.createComponent(PreviewBoxComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
+
     expect(component).toBeTruthy();
   });
 });
