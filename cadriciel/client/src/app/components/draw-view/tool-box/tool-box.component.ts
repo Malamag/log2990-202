7614d1ff43890d9121d6c7d2625281d4cffa41c6
Etienne <etienne.lescarbeault@polymtl.ca>
2020-02-23 20:39:17 -0500

    passing tests on tool box component

--- cadriciel/client/src/app/components/draw-view/tool-box/tool-box.component.ts
+++ cadriciel/client/src/app/components/draw-view/tool-box/tool-box.component.ts
@@ -4,66 +4,60 @@ import { toolsItems } from '../../../functionality';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material/icon';
 @Component({
-  selector: 'app-tool-box',
-  templateUrl: './tool-box.component.html',
-  styleUrls: ['./tool-box.component.scss']
+    selector: 'app-tool-box',
+    templateUrl: './tool-box.component.html',
+    styleUrls: ['./tool-box.component.scss'],
 })
-
 export class ToolBoxComponent implements OnInit {
-  funcTools = toolsItems;
+    funcTools = toolsItems;
 
-  activeButton: any;
+    activeButton: any;
 
-  disableUndo: boolean;
-  disableRedo: boolean;
-  // I doubt if we can delete these two
-  @ViewChild('toolsOptionsRef', { static: false }) navBarRef: ElementRef
-  selectingToolsMap = new Map();
+    disableUndo: boolean;
+    disableRedo: boolean;
+    // I doubt if we can delete these two
+    @ViewChild('toolsOptionsRef', { static: false }) navBarRef: ElementRef;
+    selectingToolsMap = new Map();
 
-  constructor(public interactionService: InteractionService, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    constructor(public interactionService: InteractionService, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+        iconRegistry.addSvgIcon('redo', sanitizer.bypassSecurityTrustResourceUrl('../../../assets/images/redo.svg'));
+        iconRegistry.addSvgIcon('undo', sanitizer.bypassSecurityTrustResourceUrl('../../../assets/images/undo.svg'));
 
-    iconRegistry.addSvgIcon(
-      'redo',
-      sanitizer.bypassSecurityTrustResourceUrl('../../../assets/images/redo.svg'));
-    iconRegistry.addSvgIcon(
-      'undo',
-      sanitizer.bypassSecurityTrustResourceUrl('../../../assets/images/undo.svg'));
-
-    this.selectingToolsMap.set('1', 'Rectangle');
-    this.selectingToolsMap.set('c', 'Crayon');
-    this.selectingToolsMap.set('w', 'Pinceau');
-    this.selectingToolsMap.set('l', 'Ligne');
-    this.selectingToolsMap.set('2', 'Ellipse');
-    this.selectingToolsMap.set('3', 'Polygone');
-    this.selectingToolsMap.set('u', 'Annuler');
-    this.selectingToolsMap.set('r', 'Refaire');
-    this.disableUndo = true;
-    this.disableRedo = true;
-  }
-  @HostListener('document: keydown', ['$event'])
-  updateBoard(event: KeyboardEvent) {
-    // keyCode 90 for z
-    if (event.ctrlKey && event.keyCode === 90) {
-      if (event.shiftKey) {
-        this.buttonAction(this.selectingToolsMap.get('r'));
-      } else {
-        this.buttonAction(this.selectingToolsMap.get('u'));
-      }
-    } else if (this.selectingToolsMap.has(event.key)) {
-      this.buttonAction(this.selectingToolsMap.get(event.key))
+        this.selectingToolsMap.set('1', 'Rectangle');
+        this.selectingToolsMap.set('c', 'Crayon');
+        this.selectingToolsMap.set('w', 'Pinceau');
+        this.selectingToolsMap.set('l', 'Ligne');
+        this.selectingToolsMap.set('2', 'Ellipse');
+        this.selectingToolsMap.set('3', 'Polygone');
+        this.selectingToolsMap.set('u', 'Annuler');
+        this.selectingToolsMap.set('r', 'Refaire');
+        this.disableUndo = true;
+        this.disableRedo = true;
     }
-  }
+    @HostListener('document: keydown', ['$event'])
+    updateBoard(event: KeyboardEvent) {
+        // keyCode 90 for z
+        if (event.ctrlKey && event.keyCode === 90) {
+            if (event.shiftKey) {
+                this.buttonAction(this.selectingToolsMap.get('r'));
+            } else {
+                this.buttonAction(this.selectingToolsMap.get('u'));
+            }
+        } else if (this.selectingToolsMap.has(event.key)) {
+            this.buttonAction(this.selectingToolsMap.get(event.key));
+        }
+    }
 
-  ngOnInit() {
-    this.interactionService.$enableDisableButtons.subscribe((disableContainer) => {
-      this.disableUndo = disableContainer[0]
-      this.disableRedo = disableContainer[1]
-    })
-  }
+    ngOnInit() {
+        this.interactionService.$enableDisableButtons.subscribe(disableContainer => {
+            this.disableUndo = disableContainer[0];
+            this.disableRedo = disableContainer[1];
+        });
+    }
 
-  buttonAction(name: string) { // on click, emit the selected tool name
-    this.interactionService.emitSelectedTool(name);
-    this.activeButton = name;
-  }
-
+    buttonAction(name: string) {
+        // on click, emit the selected tool name
+        this.interactionService.emitSelectedTool(name);
+        this.activeButton = name;
+    }
 }
