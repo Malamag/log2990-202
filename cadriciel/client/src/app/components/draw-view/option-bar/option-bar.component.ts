529d4aa599de5bf52db61270d0a9a35e702e8892
Etienne <etienne.lescarbeault@polymtl.ca>
2020-02-24 13:39:43 -0500

    shortcut to open export

--- cadriciel/client/src/app/components/draw-view/option-bar/option-bar.component.ts
+++ cadriciel/client/src/app/components/draw-view/option-bar/option-bar.component.ts
@@ -4,57 +4,62 @@ import { Canvas } from 'src/app/models/Canvas.model';
 import { KeyboardHandlerService } from 'src/app/services/keyboard-handler/keyboard-handler.service';
 import { InteractionService } from 'src/app/services/service-interaction/interaction.service';
 import { ModalWindowService } from 'src/app/services/window-handler/modal-window.service';
-import {menuItems} from '../../../functionality';
+import { menuItems } from '../../../functionality';
 import { ExportFormComponent } from '../../export-form/export-form.component';
 import { NewDrawComponent } from '../../new-draw/new-draw.component';
 import { UserManualComponent } from '../../user-manual/user-manual.component';
 
 @Component({
-  selector: 'app-option-bar',
-  templateUrl: './option-bar.component.html',
-  styleUrls: ['./option-bar.component.scss']
+    selector: 'app-option-bar',
+    templateUrl: './option-bar.component.html',
+    styleUrls: ['./option-bar.component.scss'],
 })
 export class OptionBarComponent implements OnInit {
-  funcMenu = menuItems;
-  canvasSub: Subscription;
-  currentCanvas: Canvas;
+    funcMenu = menuItems;
+    canvasSub: Subscription;
+    currentCanvas: Canvas;
 
-  constructor(public winService: ModalWindowService, public interaction: InteractionService) {
-
-    window.addEventListener('keydown', (e) => {
-      this.setShortcutEvent(e);
-    });
-  }
-
-  ngOnInit() {}
-
-  setShortcutEvent(e: KeyboardEvent) {
-    const O_KEY = 79; // keycode for letter o
-
-    const kbHandler: KeyboardHandlerService = new KeyboardHandlerService();
-    kbHandler.logkey(e);
-
-    if (kbHandler.ctrlDown && (kbHandler.keyCode === O_KEY)) { // ctrl+o opens the form!
-        this.openNewDrawForm();
-        e.preventDefault(); // default behavior (file menu) prevented
-      }
-  }
-
-  openNewDrawForm() {
-    if (confirm('Un dessin est déjà en cours. Voulez-vous continuer?')) {
-      this.winService.openWindow(NewDrawComponent);
+    constructor(public winService: ModalWindowService, public interaction: InteractionService) {
+        window.addEventListener('keydown', e => {
+            this.setShortcutEvent(e);
+        });
     }
-  }
 
-  openUserGuide() {
-    this.winService.openWindow(UserManualComponent)
-  }
+    ngOnInit() {}
 
-  openExportForm() {
-    this.winService.openWindow(ExportFormComponent);
-  }
+    setShortcutEvent(e: KeyboardEvent) {
+        const O_KEY = 79; // keycode for letter o
+        const E_KEY = 69;
 
-  sendSigKill() {
-    this.interaction.emitCancel(true)
-  }
+        const kbHandler: KeyboardHandlerService = new KeyboardHandlerService();
+        kbHandler.logkey(e);
+
+        if (kbHandler.ctrlDown && kbHandler.keyCode === O_KEY) {
+            // ctrl+o opens the form!
+            this.openNewDrawForm();
+        }
+
+        if (kbHandler.ctrlDown && kbHandler.keyCode === E_KEY) {
+            this.openExportForm();
+        }
+        e.preventDefault(); // default behavior (file menu) prevented
+    }
+
+    openNewDrawForm() {
+        if (confirm('Un dessin est déjà en cours. Voulez-vous continuer?')) {
+            this.winService.openWindow(NewDrawComponent);
+        }
+    }
+
+    openUserGuide() {
+        this.winService.openWindow(UserManualComponent);
+    }
+
+    openExportForm() {
+        this.winService.openWindow(ExportFormComponent);
+    }
+
+    sendSigKill() {
+        this.interaction.emitCancel(true);
+    }
 }
