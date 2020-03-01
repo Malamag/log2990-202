da5bb00e29b909cc262c951425082f5d24e16c8a
Etienne <etienne.lescarbeault@polymtl.ca>
2020-03-01 13:55:21 -0500

    added image filter service

--- cadriciel/client/src/app/components/gallery/gallery.component.ts
+++ cadriciel/client/src/app/components/gallery/gallery.component.ts
@@ -1,23 +1,23 @@
-import { Component, ElementRef, OnInit/*, Renderer2*/, ViewChild} from '@angular/core';
-import {fakeImages} from './fake_images'
+import { Component, ElementRef, OnInit /*, Renderer2*/, ViewChild } from '@angular/core';
+import { fakeImages } from './fake_images';
 @Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+    selector: 'app-gallery',
+    templateUrl: './gallery.component.html',
+    styleUrls: ['./gallery.component.scss'],
 })
-export class GalleryComponent implements OnInit{
-  fakeImage = fakeImages;
-  filter: string;
-  filterContainer: string[];
-  @ViewChild('filterContainer', {static: false}) filters: ElementRef;
-  constructor(/*private render: Renderer2*/) {
-    this.filter = '';
-    this.filterContainer = [];
-  }
+export class GalleryComponent implements OnInit {
+    fakeImage = fakeImages;
+    filter: string;
+    filterContainer: string[];
+    @ViewChild('filterContainer', { static: false }) filters: ElementRef;
+    constructor(/*private render: Renderer2*/) {
+        this.filter = '';
+        this.filterContainer = [];
+    }
 
-  ngOnInit() {}
+    ngOnInit() {}
 
-  /*addFilter() {
+    /*addFilter() {
     const newInput = this.render.createElement('input');
     this.render.setAttribute(newInput, 'type', 'text');
     this.render.appendChild(this.filters.nativeElement, newInput);
