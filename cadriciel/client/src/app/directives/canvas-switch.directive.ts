c74ec91ea79181b38af8ed6ca32a839e0186f952
Etienne <etienne.lescarbeault@polymtl.ca>
2020-03-06 19:48:53 -0500

    svg<-->canvas display toggle

--- cadriciel/client/src/app/directives/canvas-switch.directive.ts
+++ cadriciel/client/src/app/directives/canvas-switch.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef, Input, AfterViewInit } from '@angular/core';
 import { ExportService } from '../services/exportation/export.service';
+import { InteractionService } from '../services/service-interaction/interaction.service';
 
 @Directive({
     selector: '[appCanvasSwitch]',
@@ -8,23 +9,41 @@ export class CanvasSwitchDirective implements AfterViewInit {
     @Input('appCanvasRef') canvas: HTMLCanvasElement;
 
     imageToConvert: SVGElement;
+    showCanvas: boolean = false;
+    private readonly noDisplay: string = 'none';
+    private readonly display: string = 'block';
 
-    constructor(private element: ElementRef, private exService: ExportService) {
+    constructor(private element: ElementRef, private exService: ExportService, private itService: InteractionService) {
         this.imageToConvert = this.element.nativeElement;
     }
 
     ngAfterViewInit() {
-        //if (this.canvas) console.log(this.canvas);
         /**
          * Recieves a signal to transform the SVG to a canvas
          */
+        this.canvas.style.display = this.noDisplay;
+        this.imageToConvert.style.display = this.display;
 
-        // no name and type set. as optionnal attributes. We dont want to download it
-        this.exService.exportInCanvas(this.imageToConvert, this.canvas);
-        this.toggleSvgCanvas();
+        this.itService.$convertSvg2Canvas.subscribe((toCanvas: boolean) => {
+            // no name and type set. as optionnal attributes. We dont want to download it
+            if (toCanvas) {
+                this.exService.exportInCanvas(this.imageToConvert, this.canvas);
+                this.showCanvas = toCanvas;
+            } else {
+                this.showCanvas = !toCanvas;
+            }
+
+            this.toggleSvgCanvas();
+        });
     }
 
     toggleSvgCanvas() {
-        //  const DISPLAY_MODE: string = 'none';
+        if (this.showCanvas) {
+            this.canvas.style.display = this.display;
+            this.imageToConvert.style.display = this.noDisplay;
+        } else {
+            this.canvas.style.display = this.display;
+            this.imageToConvert.style.display = this.noDisplay;
+        }
     }
 }
