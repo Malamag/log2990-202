f0976d4644cbb41e3a58db25108226413da1e509
Etienne <etienne.lescarbeault@polymtl.ca>
2020-03-07 21:12:16 -0500

    pipette functionnal with color picker update

--- cadriciel/client/src/app/services/draw-tool/pipette.service.ts
+++ cadriciel/client/src/app/services/draw-tool/pipette.service.ts
@@ -3,7 +3,6 @@ import { InputObserver } from './input-observer';
 
 import { Point } from './point';
 import { ColorPickingService } from '../colorPicker/color-picking.service';
-import { KeyboardHandlerService } from '../keyboard-handler/keyboard-handler.service';
 
 @Injectable({
     providedIn: 'root',
@@ -12,6 +11,7 @@ export class PipetteService extends InputObserver {
     htmlCanvasEl: HTMLCanvasElement;
     cPick: ColorPickingService;
     clickedColor: Uint8ClampedArray;
+    colorStr: string = '';
     constructor(selected: boolean, htmlCanvasEl: HTMLCanvasElement, colorPicking: ColorPickingService) {
         super(selected);
         this.htmlCanvasEl = htmlCanvasEl;
@@ -19,16 +19,18 @@ export class PipetteService extends InputObserver {
     }
 
     down(position: Point, insideWorkspace?: boolean | undefined, isRightClick?: boolean): void {
-        console.log(position);
-        let ctx = this.htmlCanvasEl.getContext('2d');
-        if (ctx) {
-            this.clickedColor = ctx.getImageData(position.x, position.y, 1, 1).data;
-            console.log(this.buildImageData());
+        const CTX = this.htmlCanvasEl.getContext('2d');
+
+        if (CTX) {
+            this.clickedColor = CTX.getImageData(position.x, position.y, 1, 1).data;
+            this.colorStr = this.buildImageData();
         }
-        if (isRightClick) {
-            console.log('allo from rightclick');
+
+        if (this.colorStr) {
+            this.emitSelectedColor(this.colorStr, isRightClick);
         }
     }
+
     up(position: Point, insideWorkspace?: boolean | undefined): void {}
     move(position: Point): void {
         /**
@@ -45,13 +47,27 @@ export class PipetteService extends InputObserver {
         return str;
     }
 
-    emitSelectedColor() {}
+    emitSelectedColor(color: string, rightClick?: boolean) {
+        const BACK_COLOR: string = this.cPick.cData.backgroundColor;
+        const PRIM_COLOR = this.cPick.cData.primaryColor;
+        const SEC_COLOR = this.cPick.cData.secondaryColor;
 
-    doubleClick(): void {}
+        if (rightClick) {
+            this.cPick.colors = { primColor: PRIM_COLOR, secColor: color, backColor: BACK_COLOR };
+            // this.cPick.colors.primColor = color;
+        } else {
+            this.cPick.colors = { primColor: color, secColor: SEC_COLOR, backColor: BACK_COLOR };
+        }
+        this.cPick.emitColors();
+
+        this.cPick.updateDisplay(color);
+    }
+
+    doubleClick(): void {} // no behavior defned for the methods below
     goingOutsideCanvas(): void {}
     goingInsideCanvas(): void {}
     update(): void {}
     cancel(): void {}
-    updateDown(keyboard: KeyboardHandlerService): void {}
+    updateDown(): void {}
     updateUp(keyCode: number): void {}
 }
