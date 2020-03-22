31899f464d3ea1dbeb18ac3d475a4f0785a2d4ca
Etienne Lescarbeault <etienne.lescarbeault@polymtl.ca>
2020-03-22 10:53:31 -0400

    full test coverage on doodlefetch

--- cadriciel/client/src/app/services/doodle-fetch/doodle-fetch.service.spec.ts
+++ cadriciel/client/src/app/services/doodle-fetch/doodle-fetch.service.spec.ts
@@ -1,10 +1,11 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ElementRef } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { GridRenderService } from '../grid/grid-render.service';
 import { DoodleFetchService } from './doodle-fetch.service';
 
 describe('DoodleFetchService', () => {
     let service: DoodleFetchService;
+    let spyObj: jasmine.SpyObj<GridRenderService>;
     // tslint:disable-next-line: no-any
     let nativeElemStub: any;
     // tslint:disable-next-line: no-any
@@ -29,10 +30,14 @@ describe('DoodleFetchService', () => {
             providers: [
                 { provide: ElementRef, useValue: elementStub },
                 { provide: Node, useValue: nativeElemStub },
+
             ],
         });
         service = TestBed.get(DoodleFetchService);
         service.currentDraw = elementStub;
+        spyObj = jasmine.createSpyObj('GridRenderService', ['renderBack', 'removeGrid']);
+        // tslint:disable-next-line: no-string-literal
+        service['gService'] = spyObj;
     });
 
     it('should be created', () => {
@@ -52,24 +57,31 @@ describe('DoodleFetchService', () => {
     });
 
     it('should remove the grid when getting the doodle and put it back', () => {
-        // tslint:disable-next-line: no-string-literal
-        const spyRem = spyOn(service['gService'], 'removeGrid');
-        // tslint:disable-next-line: no-string-literal
-        const spyRenderBack = spyOn(service['gService'], 'renderBack');
         service.getSVGElementFromRef = jasmine.createSpy().and.returnValue(nativeElemStub);
         service.getDrawingWithoutGrid();
-        expect(spyRem).toHaveBeenCalledBefore(spyRenderBack);
-        expect(service.getSVGElementFromRef).toHaveBeenCalledBefore(spyRenderBack);
-        expect(spyRenderBack).toHaveBeenCalled();
+        expect(spyObj.removeGrid).toHaveBeenCalledBefore(spyObj.renderBack);
+        expect(service.getSVGElementFromRef).toHaveBeenCalledBefore(spyObj.renderBack);
+        expect(spyObj.renderBack).toHaveBeenCalled();
     });
 
     it('should remove and put the grid back when getting the doodle string', () => {
-        // tslint:disable-next-line: no-string-literal
-        const gridRemSpy = spyOn(service['gService'], 'removeGrid');
-        // tslint:disable-next-line: no-string-literal
-        const gridRenderSpy = spyOn(service['gService'], 'renderBack');
-        service.getDrawingWithoutGrid();
-        expect(gridRemSpy).toHaveBeenCalledBefore(gridRenderSpy);
-        expect(gridRenderSpy).toHaveBeenCalled();
+
+        service.currentDraw = elementStub;
+
+        service.getDrawingDataNoGrid();
+        expect(spyObj.removeGrid).toHaveBeenCalledBefore(spyObj.renderBack);
+        expect(spyObj.renderBack).toHaveBeenCalled();
+    });
+
+    it('should fill the innerHTML array for SVGData interface', () => {
+        service.currentDraw = elementStub;
+        const TEST_W = 200;
+        const TEST_H = 400;
+        const TEST_COLOR = '#ffffffff';
+        service.widthAttr = TEST_W;
+        service.heightAttr = TEST_H;
+        service.backColor = TEST_COLOR;
+        const TEST_RET = service.getDrawingDataNoGrid();
+        expect(TEST_RET.innerHTML.length).toEqual(nativeElemStub.children.length);
     });
 });
