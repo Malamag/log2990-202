ae80eba864be14f52bb4620b781242baf133607f
Etienne Lescarbeault <etienne.lescarbeault@polymtl.ca>
2020-03-22 16:02:21 -0400

    added output decorator for mini color picker

--- cadriciel/client/src/app/components/new-draw/new-draw.component.ts
+++ cadriciel/client/src/app/components/new-draw/new-draw.component.ts
@@ -1,16 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ColorConvertingService } from 'src/app/services/colorPicker/color-converting.service';
 import { ModalWindowService } from 'src/app/services/window-handler/modal-window.service';
 import { CanvasBuilderService } from '../../services/new-doodle/canvas-builder.service';
-import { colorData } from '../color-picker/color-data';
 
-const OFFSET_Y = 10;
-const OFFSET_X = 25;
-const SV_MAX_VALUE = 100;
-const CURSOR_WIDTH = 6;
-const DEFAULT_VALUE = 100;
 @Component({
   selector: 'app-new-draw',
   templateUrl: './new-draw.component.html',
@@ -19,39 +12,17 @@ const DEFAULT_VALUE = 100;
 
 export class NewDrawComponent implements OnInit {
   // tslint:disable-next-line: typedef
-  cData = colorData;
   newDrawForm: FormGroup;
   width: number;
   height: number;
   color: string;
-  offsetY: number;
-  offsetX: number;
-  svMaxValue: number;
-  hueCursorWidth: number;
-  hue: number;
-  saturation: number;
-  value: number;
-  svCursorPos: {};
-  isHueSelecting: boolean;
-  isSVSelecting: boolean;
   inputEntered: boolean;
 
   constructor(
     private formBuilder: FormBuilder,
     private canvasBuilder: CanvasBuilderService,
     private winService: ModalWindowService,
-    private router: Router,
-    private colorConvert: ColorConvertingService) {
-    this.offsetY = OFFSET_Y;
-    this.offsetX = OFFSET_X;
-    this.svMaxValue = SV_MAX_VALUE;
-    this.hueCursorWidth = CURSOR_WIDTH;
-    this.hue = 0;
-    this.saturation = 0;
-    this.value = DEFAULT_VALUE;
-    this.svCursorPos = { x: this.saturation, y: this.svMaxValue - this.value };
-    this.isHueSelecting = false;
-    this.isSVSelecting = false;
+    private router: Router) {
   }
 
   ngOnInit(): void {
@@ -119,51 +90,9 @@ export class NewDrawComponent implements OnInit {
     return this.newDrawForm.get('canvColor');
   }
 
-  updateColor(): void {
-    // slice (1) removes the '#'
-    this.color = this.colorConvert.hsvToHex(this.hue, this.saturation / this.svMaxValue, this.value / this.svMaxValue).slice(1);
+  updateColor(color: string): void {
+    this.color = color;
     this.newDrawForm.patchValue({ canvColor: this.color }); // updates value for form
   }
 
-  onMouseDownHue(event: MouseEvent): void {
-    this.isHueSelecting = true;
-    this.hueSelect(event);
-  }
-
-  onMouseDownSV(event: MouseEvent): void {
-    this.isSVSelecting = true;
-    this.svSelect(event);
-  }
-
-  onMouseUp(): void {
-    this.isHueSelecting = false;
-    this.isSVSelecting = false;
-  }
-
-  hueSelect(event: MouseEvent): void {
-    if (this.isHueSelecting) {
-      this.hue = Math.round((event.offsetY - this.offsetY) * (this.cData.MAX_HUE_VALUE / this.svMaxValue));
-      this.updateColor();
-    }
-  }
-
-  svSelect(event: MouseEvent): void {
-    if (this.isSVSelecting) {
-      this.saturation = event.offsetX - this.offsetX;
-      this.value = this.svMaxValue - (event.offsetY - this.offsetY);
-      this.updateColor();
-    }
-  }
-
-  get hueCursorStyles(): {} {
-    return {
-      transform: 'translateY(' +
-        ((Math.round(this.hue * (this.svMaxValue / this.cData.MAX_HUE_VALUE))) +
-          (this.offsetY - this.hueCursorWidth / 2)) + 'px)'
-    };
-  }
-
-  get svCursorStyles(): {} {
-    return { transform: 'translate(' + this.saturation + 'px,' + (this.svMaxValue - this.value) + 'px)' };
-  }
 }
