95c75f393b30b764f9391d74a6448e92f29e0f4d
malamag <maxym.lamothe@polymtl.ca>
2020-03-24 10:21:59 -0400

    mock db and test json are functional. Add some test

--- cadriciel/server/app/services/database.service.spec.ts
+++ cadriciel/server/app/services/database.service.spec.ts
@@ -21,7 +21,7 @@ describe('Database service', () => {
     //let client: MongoClient;
     //let assert = require('assert');
 
-    beforeEach(async () => {
+    before(async () => {
         container = new inversify.Container();
         container.bind(Types.DatabaseService).to(DatabaseService);
         dbService = container.get<DatabaseService>(Types.DatabaseService);
@@ -44,51 +44,77 @@ describe('Database service', () => {
                     .catch(() => { console.log('Fail to connect to client') });
             })
             .catch(() => { console.log('Fail to get db url') });
-        console.log(dbService.collection);
-        //testCollection.remove({});
-
+        dbService.jsonFile = '../jsonTest.json';
+        const svg: SVGData = { height: '0', width: '0', innerHTML: ['caucue'], bgColor: 'red' };
+        const imageData: ImageData = { id: '1', svgElement: svg, name: 'ok', tags: ['ok'] };
+        await dbService.saveImage(imageData);
     });
     afterEach(() => {
-        dbService.collection.remove({})
-            .then(() => {
-                console.log('Collection was clear');
-            })
-            .catch(() => {
-                console.log('Fail to clear collection');
-            });
     })
     //test connect() yes/no
     //test getAllImages() expect/spy getImages()
     //test getImagesByTags() expect stringToArray - expect/spy getImages() - tags test - expect/spy searchTag()
     //test getImages() expect matching id
+    it('should expect a lenght of one for a seach of one existing image', async () => {
+        const metaData: MetaData[] = [{ id: '1', name: 'ok', tags: ['ok'] }];
+        return expect(dbService.getImages(metaData)).to.have.length(1);
+    })
+    it('should expect a lenght of zero for a seach of one not existing image', async () => {
+        const metaData: MetaData[] = [{ id: '1001', name: 'ok', tags: ['ok'] }];
+        return expect(dbService.getImages(metaData)).to.have.length(0);
+    })
+    /*it('should expect a lenght of a 5 for a seach of 5 existing image', (done: Mocha.Done) => {
+        const metaData: MetaData[] = [{ id: '1001', name: '', tags: [''] }];
+        expect(dbService.getImages(metaData)).to.have.length(0);
+        done();
+    })*/
     //test searchTag()
+    it('should expect to be true if tag is include in tags array', (done: Mocha.Done) => {
+        const tag = 'al';
+        const tags = ['allo', 'red', 'lol'];
+        expect(dbService.searchTag(tag, tags)).to.be.true;
+        done();
+    })
+    it('should expect to be false if tag is not include in tags array', (done: Mocha.Done) => {
+        const tag = 'w';
+        const tags = ['allo', 'red', 'lol'];
+        expect(dbService.searchTag(tag, tags)).to.be.false;
+        done();
+    })
     //test stringToArray()
+    it('should change string to array format', (done: Mocha.Done) => {
+        const str = 'al,blue,reg,black';
+        const strArray = ['al', 'blue', 'reg', 'black'];
+        let result = dbService.stringToArray(str);
+        for (let i = 0; i < result.length; i++) {
+            expect(result[i]).to.be.equal(strArray[i]);
+        }
+        done();
+    })
     //test deleteImageById()
+    /*it('should delete the image with the choosen id', async () => {
+        //const svg: SVGData = { height: '0', width: '0', innerHTML: ['caucue'], bgColor: 'red' };
+        //const imageData: ImageData = { id: '10', svgElement: svg, name: 'ok', tags: ['ok'] };
+        //await dbService.saveImage(imageData);
+        await dbService.deleteImageById('1');
+        //const metaData: MetaData[] = [{ id: '1', name: 'ok', tags: ['ok'] }];
+        return expect(dbService.collection.find({})).to.have.length(1);
+    })*/
     //test saveImage()
     //test validateImageData()
-    it('should not accept image if collection is full', async (done: Mocha.Done) => {
+    /*it('should not accept image if collection is full', async () => {
         const MAX_DATA_AMOUNT = 1000;
+        const svg: SVGData = { height: '0', width: '0', innerHTML: ['caucue'], bgColor: 'red' };
         for (let i = 0; i < MAX_DATA_AMOUNT; i++) {
-            dbService.collection.insertOne({ id: i.toString(), name: "", tags: [""] })
-                .then(() => {
-                    //console.log('Data was inserted');
-                })
-                .catch(() => {
-                    console.log('Fail to insert data');
-                });
+            let imageData: ImageData = { id: i.toString(), svgElement: svg, name: 'ok', tags: ['ok'] };
+            await dbService.saveImage(imageData);
+            for (let j = 0; j < 50; j++) {
+                console.log('waiting');
+            }
         }
-        const svg: SVGData = { height: '0', width: '0', innerHTML: [''], bgColor: '' };
-        const imageData: ImageData = { id: '0', svgElement: svg, name: '', tags: [''] };
-        dbService.validateImageData(imageData)
-            .then((data) => {
-                expect(data).to.be.null;
-                done();
-            })
-            .catch(() => {
-                console.log('Fail to validate image');
-                done();
-            })
-    })
+        const imageData: ImageData = { id: '1001', svgElement: svg, name: '', tags: [''] };
+        return await dbService.validateImageData(imageData).should.eventually.equal(null);
+    })*/
     //test validateId()
     it('should accept an unique Id', (done: Mocha.Done) => {
         const svg: SVGData = { height: '0', width: '0', innerHTML: [''], bgColor: '' };
