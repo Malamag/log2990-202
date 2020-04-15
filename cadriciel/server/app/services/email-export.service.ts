40538226101bacb4d7e22522991c886aea73cfcb
malamag <maxym.lamothe@polymtl.ca>
2020-04-15 15:21:41 -0400

    dev merge

--- cadriciel/server/app/services/email-export.service.ts
+++ cadriciel/server/app/services/email-export.service.ts
@@ -1,13 +1,18 @@
 import dotenv from 'dotenv';
 import { injectable } from 'inversify';
+
 import request from 'request';
+
 import { ImageExport } from '../../../image-export';
+
 @injectable()
 export class EmailExportService {
     constructor() {
         dotenv.config();
     }
     async export(data: ImageExport): Promise<void> {
+        let a = this.validateEmail(data.email);
+        console.log(a);
         const URL = 'https://log2990.step.polymtl.ca/email?address_validation=false&quick_return=false&dry_run=false';
         console.log('email reached server');
         const OPTIONS: request.CoreOptions = {
@@ -28,10 +33,15 @@ export class EmailExportService {
                 },
             },
         };
+        //console.log(Buffer.from(data.src.split(',')[1], 'base64'));
         request(URL, OPTIONS, (res) => {
             console.log('message : ' + res);
         });
         // req.end();
         console.log('after request');
+    }
+    validateEmail(email : string) : boolean{
+        const format = new RegExp(/\S+@\S+\.\S+/);
+        return format.test(email);
     }
 }
