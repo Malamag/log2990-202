e9eacd1e51a777ac8448a605d401e6f019d5ffb7
Etienne Lescarbeault <etienne.lescarbeault@polymtl.ca>
2020-04-16 16:27:31 -0400

    updating user-guide

--- cadriciel/client/src/app/functionality.ts
+++ cadriciel/client/src/app/functionality.ts
@@ -21,17 +21,21 @@ export const menuItems = [
         shortDescription:
             "Cette option vous permet d'ouvrir un nouvel espace de dessin selon la taille et la couleur de fond de votre choix.",
         description:
-            "La largeur et la hauteur sont en pixel et sont automatiquement générés selon la taille de votre fenêtre. Pour la couleur, la couleur blanche est par défaut, mais celle-ci peut être changé par n'importe quelle autre couleur notée en hexadécimal ou avec nos couleurs prédéfinies.",
+            `La largeur et la hauteur du dessin sont
+            automatiquement générés selon les dimensions de la fenêtre de votre navigateur.
+            Par défaut, la couleur du nouveau dessin sera blanche, mais il est possible de la redéfinir avec une nouvelle valeur hexadécimale ou encore avec l’outil de sélection de couleur situé sur le formulaire. Il est possible d’ouvrir le formulaire via le raccourci 'ctrl+O'. Notez que si le dessin en cours n’a pas été sauvegardé sur la gallerie, celui-ci sera écrasé.`,
         icon: 'add_circle',
     },
     {
         name: 'Ouvrir la galerie de dessins',
         shortcutName: 'Ouvrir',
         shortDescription:
-            `Cette option vous permez de visualiser tous les dessins que vous avez sauvegardé sur le serveur de polydessin, comme elle vous dnne la possibilité de continuer un dessin sauvegardé en appuyant sur la flèche qui se trouve sous chaque dessin.
-            Aussi, vous pouvez supprimer un dessin sauvegardé sur la base de donnée.`,
-        description: `Pour ouvrir la fenêtre d'exportation, vous pouvez appuyer appuyer sur l'icône appropriée de la barre des options,
-        comme vous pouvez l'ouvrir en faisant la touche 'ctrl+G' de votre clavier`,
+            `Cette option vous permet de visualiser tous les dessins que vous avez sauvegardé sur le serveur de PolyDessin, comme elle vous donne la possibilité de continuer un dessin sauvegardé en appuyant sur la flèche qui se trouve sous chaque dessin.
+            Aussi, vous pouvez supprimer un dessin sauvegardé sur la base de données ou encore rechercher l'un d'entres eux à l'aide d'étiquettes.`,
+        description: `Pour ouvrir la fenêtre d'exportation, vous pouvez appuyer sur l'icône approprié de la barre d'options,
+        comme vous pouvez l'ouvrir en faisant la touche 'ctrl+G' de votre clavier. Si des dessins précédemment sauvegardés sont présents sur le serveur, ils seront automatiquement affichés sous forme
+        de fenêtre de prévisualisation en vous laissant le choix de supprimer ou de continuer celui-ci. Cette dernière option écrasera le dessin courant dans le cas où il n'a pas été sauvegardé au préalable.
+        Enfin, il est possible de rechercher un dessin à l'aide d'étiquettes via la barre de filtrage située au haut de la gallerie.`,
         icon: 'camera',
     },
     {
@@ -46,26 +50,33 @@ export const menuItems = [
         name: 'Continuer un dessin',
         shortcutName: 'Continuer',
         shortDescription: 'Cette option vous laisse continuer votre dernier dessin sauvegardé automatiquement.',
-        description: 'Disponible sous peu!',
+        description: `
+        Cette option vous permet de continuer le tout dernier dessin effectué avec PolyDessin et ce, même si vous fermez l'application et le navigateur!
+        L'application sauvegardera automatiquement le dessin en cours au fur et à mesure que vous dessinez; aucune action n'est donc requise de votre part.
+        L'option est disponible au point d'entrée de l'application, et le bouton ne sera disponible que s'il existe un dessin pouvant être continué. Une fois choisi,
+        l'application chargera automatiquement le dessin sur la planche.`,
         icon: 'arrow_forward',
     },
     {
         name: 'Exporter un dessin',
         shortcutName: 'Exporter',
-        shortDescription: `Permet dexporter un dessin localement selon 3 différents formats: SVG, PNG ou JPG.
-         Aussi, elle vous permet d'appliquer des filtres sur le dessin que vous voulez exporter. En plus cette fonctionnalité vous donne la possibilité d'envoyer
-         un dessin par courriel en entrant le courriel de la personne recevante.`,
-        description: `Pour ouvrir la fenêtre d'exportation, vous pouvez appuyer appuyer sur l'icône appropriée de la barre des options,
-        comme vous pouvez l'ouvrir en faisant la touche 'ctrl+E' de votre clavier`,
+        shortDescription: `Permet d'exporter un dessin localement selon 3 différents formats: SVG, PNG ou JPG.
+         Aussi, elle vous permet d'appliquer un choix parmi 5 filtres sur le dessin que vous voulez exporter. De plus, cette fonctionnalité vous donne la possibilité d'envoyer
+         un dessin par courriel en entrant l'adresse courriel de la personne récipiendaire.`,
+        description: `Pour ouvrir la fenêtre d'exportation, vous pouvez appuyer sur l'icône appropriée de la barre des options,
+        comme vous pouvez l'ouvrir en faisant la touche 'ctrl+E' de votre clavier. Vous verrez ensuite une fenêtre de prévisualisation du dessin en question,
+        avec des options permettant d'entrer le nom du dessin, le format d'exportation de même que 5 filtres optionnels. Une fois le bouton pressé, l'application
+        lancera un téléchargement local du dessin, à moins que vous aviez choisi un envoi par courriel. Le cas échéant, le dessin ira au destinaraire entré!`,
         icon: 'cloud_download',
     },
     {
         name: 'Sauvegarder dessin',
         shortcutName: 'sauvegarde',
         shortDescription: `Permet de sauvegarder un dessin sur la base de données de l'application. Lors de la sauvegarde, vous devez entrer un nom non vide
-        à votre dessin, comme vous pouvez y mettre des étiquettes`,
+        à votre dessin, comme vous pouvez y mettre des étiquettes permettant d'identifier plus facilement votre dessin.`,
         description: `Pour ouvrir la fenêtre de sauvegarde, vous pouvez appuyer appuyer sur l'icône appropriée de la barre des options,
-        comme vous pouvez l'ouvrir en faisant la touche 'ctrl+S' de votre clavier`,
+        comme vous pouvez l'ouvrir en faisant la touche 'ctrl+S' de votre clavier. Une fois le formulaire de sauvegarde ouvert, vous serez invité à rentrer un nom pour le dessin
+        et, en option, des étiquettes qui vous permettront de retrouver le dessin en question sur la gallerie via l'option de filtrage.`,
         icon: 'save',
     },
 ];
@@ -75,33 +86,41 @@ export const toolsItems = [
         name: 'Sélectionner',
         shortcutName: 'Sélectionner',
         shortDescription:
-            "Grâce à cet outil, il vous est possible de sélectionner un objet sur l'espace de dessin et de le modifier. En effet, il vous est possible de le déplacer, de le pivoter ou même de l'agrandir ou le rétrécir.",
+            "Grâce à cet outil, il vous est possible de sélectionner un objet sur l'espace de dessin et de le modifier. En effet, il vous est possible de le déplacer et de le pivoter.",
         description: `Pour choisir cette option, vous pouvez sélectionner l'outil via la barre des outils en appuyant sur l'icône approprié,
-        comme vous pouvez l'utiliser en appuyant sur la touche 'S' de votre clavier.`,
+        comme vous pouvez l'utiliser en appuyant sur la touche 'S' de votre clavier. Avec l'outil en main, vous pouvez cliquer sur un seul objet sur la planche à dessin
+        ou encore en sélectionner plusieurs en effectuant un clic gauche et en faisant glisser la souris pour utiliser le rectangle de sélection, puis en relâchant le bouton de la souris pour sélectionner les items. À l'inverse,
+        un clic droit avec cette option crée un rectangle de sélection qui applique un traitement inverse. Il vous sera alors possible de déselectionner les objets déjà sélectionnés ou de sélectionner ceux ne l'étant pas.
+        Une fois la sélection complétée, une boîte englobant tous les objets pris en compte apparaîtra. Il sera ensuite possible de déplacer cette sélection avec les flèches du clavier ou avec la souris et de faire tourner cette sélection.
+        Si la touche 'shift' est pressée, chaque objet tournera autour de son propre centre.
+        Enfin, appuyer sur 'ctrl+A' sélectionnera tous les objets`,
         icon: 'cursor',
     },
     {
         name: 'Crayon',
         shortcutName: 'Crayon',
-        shortDescription: 'Le crayon vous permet de dessiner un trait à l’écran.',
+        shortDescription: 'Le crayon vous permet de dessiner un trait simple sur la planche à dessin.',
         description:
-            'Après sa sélection, Il vous suffit de maintenir le clic gauche en vous déplaçant dans n’importe quelle direction dans l’aire de dessin.',
+            `Après sa sélection, Il vous suffit de maintenir le clic gauche en vous déplaçant dans n’importe quelle
+            direction dans l’aire de dessin. Un trait simple de la couleur et grandeur choisies apparaîtra. Vous pouvez sélectionner le crayon
+            via l'icône appropriée dans la boîte à outils ou encore en pressant sur la touche 'C'.`,
         icon: 'pencil',
     },
     {
         name: 'Pinceau',
         shortcutName: 'Pinceau',
         shortDescription: 'Le pinceau vous permet de tracer comme un crayon, mais avec une texture différente.',
-        description: 'Il vous est possible de modifier l’épaisseur et la texture du trait dans son panneau de configuration.',
+        description: `Le pinceau permet, tout comme le crayon, de dessiner un trait à l'écran mais en offrant un choix de 5 textures différentes.
+        Étant disponible via la boîte à outils, il est possible de le sélectionner avec la touche 'B' du clavier.`,
         icon: 'brush',
     },
     {
         name: 'Aérosol',
         shortcutName: 'Aérosol',
         shortDescription:
-            'Cet outil vous permet de simuler un jet de peinture en aérosol en appuyant sur le clic gauche de la souris. En effet, plusieurs points de couleurs apparaissent lors du clic et le nombre de points augmentent si l’on maintient la touche.',
+            'Cet outil vous permet de simuler un jet de peinture en aérosol en appuyant sur le clic gauche de la souris. En effet, plusieurs points de couleurs apparaissent lors du clic et le nombre de points augmente si l’on maintient le clic.',
         description: `Pour  utiliser l'aérosol, vous pouvez sélectionner l'outil en appuyant sur l'icône appropriée via la barre des outils,
-        comme vous pouvez la sélectionner en appuyant sur la touche 'A' de votre clavier.`,
+        comme vous pouvez la sélectionner en appuyant sur la touche 'A' de votre clavier, et ensuite spécifier l'intensité et le diamètre du jet.`,
         icon: 'spray',
     },
     {
@@ -109,16 +128,19 @@ export const toolsItems = [
         shortcutName: 'Efface',
         shortDescription: 'L’efface supprimera tout objet auquel il sera en contact lors du clic de souris.',
         description: `Pour choisir cette option, vous pouvez sélectionner l'outil via la barre des outils en appuyant sur l'icône approprié,
-        comme vous pouvez l'utiliser en appuyant sur la touche 'E' de votre clavier.`,
+        comme vous pouvez l'utiliser en appuyant sur la touche 'E' de votre clavier. Les objets entrant en contact avec l'efface se verront dotés d'un contour rouge.`,
         icon: 'eraser',
     },
     {
         name: 'Ligne',
         shortcutName: 'Ligne',
         shortDescription:
-            'Cet outil vous permet de créer des lignes droites. Il suffit de cliquer à un endroit sur le canvas, puis de cliquer à un autre endroit ensuite pour voir apparaître une ligne droite entre ces deux points. En cliquant de nouveau, une ligne se créera entre les deux derniers points et ainsi de suite.',
+            'Cet outil vous permet de créer des lignes droites. Il suffit de cliquer à un endroit sur le canevas, puis de cliquer à un autre endroit ensuite pour voir apparaître une ligne droite entre ces deux points. En cliquant de nouveau, une ligne se créera entre les deux derniers points et ainsi de suite.',
         description:
-            'Pour forcer des angles précis, la touche shift peut être appuyé afin de créer des lignes ayant des angles faisant des bonds de 45 degrés ; soit des angles de 0, 45, 90, 135, etc. De plus, si la touche retour arrière (Backspace) est appuyé, le dernier point sera supprimé. Aussi, la touche d’espacement (Escape) vous permet de supprimer la dernière ligne créée.',
+            `Pour forcer des angles précis, la touche shift peut être appuyée afin de créer des lignes ayant des angles
+            faisant des bonds de 45 degrés ; soit des angles de 0, 45, 90, 135, etc. De plus, si la touche retour arrière (Backspace) est appuyé,
+            le dernier point sera supprimé. Aussi, la touche d’échappement (Escape) vous permet de supprimer la dernière ligne créée. Enfin, il est possible
+            de doter la ligne de points de jonction de diamètre modifiable.`,
         icon: 'line',
     },
     {
@@ -174,8 +196,8 @@ export const toolsItems = [
         shortcutName: 'App. de couleur',
         shortDescription:
             `Cette outil vous permet de changer la couleur secondaire et primaire d'un objet déjà créé,
-             Pour changer la couleur primaire en faisant un clique gauche de votre souris.
-             Par contre, si vous voulez changer la couleur secondaire c'est en faisant un clique droit de votre souris.`,
+             Pour changer la couleur primaire en faisant un clic gauche de votre souris.
+             Par contre, si vous voulez changer la couleur secondaire c'est en faisant un clic droit de votre souris.`,
         description:
             `Vous pouvez utiliser cette outil via la barre des outils en sélectionnant l'icône appropriée
             ou en appuyant sur la touche 'R' de votre clavier.`,
